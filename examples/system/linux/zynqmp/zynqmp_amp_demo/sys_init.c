ca4e728ffb1bdefd726610b49744a005cd6f825e
Ben Levinsky <ben.levinsky@xilinx.com>
2021-02-08 15:15:07 -0800

    libmetal: amp_demo: linux: update for Versal hardware support
    Xilinx Versal hardware's platform manager requires certain nodes to be
    requested via a client API before the node is powered on.
    
    One such node which the libmetal_amp_demo demo needs is the TTC0 hardware
    block. Update to make sure this block is powered on via client API call
    before attempting to use this in demos.
    
    Without this line, the TTC0 block is not powered on and all read
    values may be 0.
    
    Signed-off-by: Ben Levinsky <ben.levinsky@xilinx.com>

--- examples/system/linux/zynqmp/zynqmp_amp_demo/sys_init.c
+++ examples/system/linux/zynqmp/zynqmp_amp_demo/sys_init.c
@@ -1,11 +1,12 @@
 
 /*
- * Copyright (c) 2016, Xilinx Inc. and Contributors. All rights reserved.
+ * Copyright (c) 2021, Xilinx Inc. and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <metal/sys.h>
 #include "common.h"
@@ -18,6 +19,15 @@ int sys_init()
 	ret = metal_init(&init_param);
 	if (ret)
 		LPERROR("Failed to initialize libmetal\n");
+#ifdef versal
+	/* ensure TTC0 is powered on before using it in demos. */
+	printf("\r\nLinux:> request TTC0 node from Xilinx Platform Manager.\r\n");
+	ret = system("echo pm_request_node 0x18224024 > /sys/kernel/debug/zynqmp-firmware/pm");
+	if (ret < 0) {
+		perror("Failed to request TTC0 from Xilinx Platform Manager..\n");
+		return -EINVAL;
+	}
+#endif
 	return ret;
 }
 
